cc014eba5c541886a0372ce8ba415bd77f680140
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-30 21:40:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require 'pry'
+
+def line(array)
+  #binding.pry
+  puts "The line is currently empty."
+end
+
+# def line(katz_deli)
+#   if katz_deli.length == 0
+#     puts "The line is currently empty."
+#   else katz_deli.length >= 1
+#     katz_deli.map.with_index(1) do |other_deli, index|
+#       puts "The line is currently: #{index}. #{other_deli}"
+#     end
+#   end
+# end
+#
+# # else katz_deli.length >= 1
+# #   counter = 0
+# #   katz_deli.map.with_index(1) do |other_deli, index|
+# #     puts "The line is currently: #{index}. #{other_deli}"
+# #   counter += 1
+# #   end
+# # end
